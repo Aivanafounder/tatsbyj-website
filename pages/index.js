f14fd19978d1292dbd04e50e23e524ff31cb0db2
Aivanafounder <chris@aivana.com>
2025-03-03 11:11:15 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to TatsbyJ</h1>
+      <p>Custom Tattoo Art & Booking</p>
+      <img src="/tattoo-placeholder.jpg" alt="Tattoo Art" style={{ maxWidth: "100%", height: "auto" }} />
+      <p>More content coming soon...</p>
+    </div>
+  );
+}
